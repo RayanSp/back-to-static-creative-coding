918fc56d413a4dd98c8fdbe92532daf522f20e34
RayanSp <rayan.spall@hva.nl>
2023-12-08 11:26:34 +0100

    imported project

---
+++ src/lib/atoms/persoonImage.svelte
@@ -0,0 +1,25 @@
+<article class="persoon_foto">
+    <img
+      src="https://i1.wp.com/www.elskerollema.nl/wp-content/uploads/2016/04/elske_rollema.jpeg?w=240&ssl=1"
+      alt="Foto van persoon"
+    />
+</article>
+
+
+<style>
+    .persoon_foto {
+        align-self: center;
+        width: 15.25rem;
+        height: 20.2rem;
+}
+    img {
+        filter: blur(10px);
+        transition: filter 0.3s ease-in-out;
+        background-image: linear-gradient(to right, var(--color-blue), yellow);
+    }
+
+    .persoon_foto:hover img {
+        filter: blur(0);
+        border: 2px solid var(--color-primary);
+    }
+</style>
---
+++ src/lib/atoms/persoonTitle.svelte
@@ -0,0 +1,31 @@
+<article class="#">
+    <h1 id="my-text" class="persoon_naam">Elske Rollema</h1>
+</article>
+
+
+
+<style>
+    .persoon_naam {
+        font-weight: bold;
+        font-size: 26px;
+        padding-bottom: 1rem;
+        background: linear-gradient(to right,var(--color-blue) 20%, rgb(233, 250, 0) 30%, rgb(233, 250, 0) 70%, var(--color-blue) 80%);
+        -webkit-background-clip: text;
+        background-clip: text;
+        -webkit-text-fill-color: transparent;
+        /* text-fill-color: transparent; */
+        background-size: 500% auto;
+        animation: gradientText 5s ease-in-out infinite alternate;
+}
+        @keyframes gradientText {
+    0% {
+        background-position: 0% 50%;
+    }
+    100% {
+        background-position: 100% 50%;
+    }
+}
+
+
+
+</style>
